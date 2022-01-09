1d87bf9381b7a15c8ca29960fc2914d0a4b7b9c8
Andrew Garkavyi <andrew.gawy@gmail.com>
2022-01-09 11:12:05 +0000

    Moving away from raspi-serial to more generic serialport library

--- src/uartListener.ts
+++ src/uartListener.ts
@@ -1,42 +1,32 @@
-import { Serial } from 'raspi-serial';
-import { init } from 'raspi';
 import { Logger } from 'homebridge';
+import SerialPort from 'serialport';
+import Readline from '@serialport/parser-readline';
 
+const SERIAL_DEVICE_NAME = '/dev/serial0';
+const BAUD_RATE = 57600;
 export class UartListener {
 
+  public messageObserver: ((msg: string) => void) | undefined;
+
   private static _instance: UartListener;
-  private messageBuffer = '';
-  // private serial: Serial;
 
   private constructor(private readonly log: Logger) {
-    this.log.debug('Uart listener created');
-
-    init(() => {
-      const serial = new Serial({baudRate: 57600});
-      serial.open(() => {
-        // this.serial = serial;
-        serial.on('data', (data) => {
-          this.receiveData(data);
-        } );
+    const serialPort = new SerialPort(SERIAL_DEVICE_NAME, {baudRate: BAUD_RATE},
+      (err) => {
+        if (err) {
+          this.log.error('Unable to open serial port: ' + err);
+        } else {
+          this.log.debug(`Serial port open: ${SERIAL_DEVICE_NAME}, rate=${BAUD_RATE}`);
+        }
       });
+    const parser = serialPort.pipe(new Readline({ delimiter: '\r\n', encoding: 'ascii' }));
+    parser.on('data', (data) => {
+      this.log.debug(data);
     });
   }
 
-  receiveData(receiveData: Buffer) {
-    const message = receiveData.toString('ascii');
-    const eom_i = message.indexOf('\r\n');
-
-    if (eom_i >= 0) { // one buffer may contain parts of several commands (2 in this case)
-      this.messageBuffer += message.substring(0, eom_i + 2);
-      this.log.debug(this.messageBuffer);
-      this.messageBuffer = message.substring(eom_i + 2);
-    } else {
-      this.messageBuffer += message;
-    }
-  }
-
   public static instance(logger: Logger) {
-    // Do you need arguments? Make it a regular static method instead.
     return this._instance || (this._instance = new this(logger));
   }
+
 }
